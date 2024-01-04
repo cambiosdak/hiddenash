59ee3da7a18f605cdf0b5b2f3dc03189929b65c6
cambiosdak <116052862+cambiosdak@users.noreply.github.com>
2024-01-04 14:53:47 -0400

    Add files via upload

---
+++ helper.js
@@ -0,0 +1,166 @@
+const fs = require('fs').promises;
+const axios = require('axios');
+const crypto = require('crypto');
+
+
+function validateApiKey(apiKey) {
+    return /^[A-Za-z0-9]{64}$/.test(apiKey);
+}
+
+function validateApiSecret(apiSecret) {
+    return /^[A-Za-z0-9]{64}$/.test(apiSecret);
+}
+
+
+async function saveCredentials(credentials) {
+    try {
+        await fs.writeFile('credentials.json', JSON.stringify(credentials, null, 2));
+    } catch (error) {
+        console.error('Error saving credentials:', error);
+    }
+}
+
+async function testApiKey(apiKey, apiSecret) {
+    const baseUrl = 'https://fapi.binance.com';
+    const endpoint = '/fapi/v2/balance';
+    const timestamp = Date.now();
+
+    const queryString = `timestamp=${timestamp}`;
+    const signature = crypto
+        .createHmac('sha256', apiSecret)
+        .update(queryString)
+        .digest('hex');
+
+    const url = `${baseUrl}${endpoint}?${queryString}&signature=${signature}`;
+    const headers = {
+        'X-MBX-APIKEY': apiKey
+    };
+
+    try {
+        const response = await axios.get(url, { headers });
+        return response.status === 200;
+    } catch (error) {
+        console.error('API key test failed:', error);
+        return false;
+    }
+}
+
+async function fetchBalance(apiKey, apiSecret) {
+    // Similar setup as in testApiKey
+    const baseUrl = 'https://fapi.binance.com';
+    const endpoint = '/fapi/v2/balance';
+    const timestamp = Date.now();
+
+    const queryString = `timestamp=${timestamp}`;
+    const signature = crypto
+        .createHmac('sha256', apiSecret)
+        .update(queryString)
+        .digest('hex');
+
+    const url = `${baseUrl}${endpoint}?${queryString}&signature=${signature}`;
+    const headers = { 'X-MBX-APIKEY': apiKey };
+
+    try {
+        const response = await axios.get(url, { headers });
+        const usdtBalance = response.data.find(entry => entry.asset === 'USDT');
+
+        if (!usdtBalance) {
+            return { balance: 'Unavailable', crossUnPnl: 'Unavailable', asset: 'USDT' };
+        }
+
+        return {
+            asset: 'USDT',
+            balance: usdtBalance.balance,
+            crossUnPnl: usdtBalance.crossUnPnl
+        };
+    } catch (error) {
+        console.error('Error fetching balance:', error);
+        return { balance: 'Error', crossUnPnl: 'Error', asset: 'USDT' };
+    }
+}
+
+async function readCredentials() {
+    try {
+        const data = await fs.readFile('credentials.json', 'utf8');
+        return JSON.parse(data);
+    } catch (error) {
+        console.error('Error reading credentials:', error);
+        return [];
+    }
+}
+
+
+async function fetchOpenPositions(apiKey, apiSecret) {
+    const baseUrl = 'https://fapi.binance.com';
+    const endpoint = '/fapi/v2/positionRisk';
+    const timestamp = Date.now();
+
+    const queryString = `timestamp=${timestamp}`;
+    const signature = crypto
+        .createHmac('sha256', apiSecret)
+        .update(queryString)
+        .digest('hex');
+
+    const url = `${baseUrl}${endpoint}?${queryString}&signature=${signature}`;
+
+    try {
+        const response = await axios.get(url, {
+            headers: {
+                'X-MBX-APIKEY': apiKey
+            }
+        });
+        // Filter positions where positionAmt is not '0'
+        const openPositions = response.data.filter(position => Math.abs(parseFloat(position.positionAmt)) > 0);
+        return openPositions; // The list of filtered open positions
+    } catch (error) {
+        console.error('Error fetching open positions:', error);
+        return []; // Return an empty array in case of an error
+    }
+}
+
+async function closePosition(apiKey, apiSecret, symbol, positionAmt) {
+    const baseUrl = 'https://fapi.binance.com';
+    const endpoint = '/fapi/v1/order';
+    const timestamp = Date.now();
+    // Determine the order side: 'BUY' to close shorts, 'SELL' to close longs
+    const orderSide = parseFloat(positionAmt) > 0 ? 'SELL' : 'BUY';
+    // Ensure the quantity is positive
+    const quantity = Math.abs(parseFloat(positionAmt)).toString();
+    // Constructing the query string without encodeURIComponent
+    let queryString = `symbol=${symbol}&side=${orderSide}&type=MARKET&quantity=${quantity}&timestamp=${timestamp}&recvWindow=60000`;
+    const signature = crypto.createHmac('sha256', apiSecret).update(queryString).digest('hex');
+    const url = `${baseUrl}${endpoint}?${queryString}&signature=${signature}`;
+    let options = {
+        method: 'POST',
+        url: url,
+        headers: {
+            'X-MBX-APIKEY': `${apiKey}` // APIKEY OF ACCOUNT B
+        },
+    }
+    try {
+        const response = await axios.request(options);
+        return response.data; // The response from the API
+    } catch (error) {
+        console.error('Error placing market order:', error.response ? error.response.data : error.message);
+        return null; // Return null in case of an error
+    }
+}
+
+
+
+async function closeAllOpenPositions(apiKey, apiSecret) {
+    const openPositions = await fetchOpenPositions(apiKey, apiSecret);
+    for (const position of openPositions) {
+        const closeOrderResult = await closePosition(apiKey, apiSecret, position.symbol, position.positionAmt);
+        if (closeOrderResult) {
+            console.log(`Closed position for ${position.symbol}:`, closeOrderResult);
+            return true
+        } else {
+            console.log(`Failed to close position for ${position.symbol}`);
+            return false
+        }
+    }
+}
+
+
+module.exports = {readCredentials, fetchBalance, testApiKey, saveCredentials, validateApiKey, validateApiSecret, fetchOpenPositions, closePosition, closeAllOpenPositions}
---
+++ index3.js
@@ -0,0 +1,196 @@
+const { Telegraf, Scenes, session, Markup  } = require('telegraf');
+const { WizardScene } = Scenes;
+const {readCredentials, fetchBalance, testApiKey, saveCredentials, validateApiKey, validateApiSecret, closeAllOpenPositions} = require('./helper3')
+const bot = new Telegraf('6661256663:AAGZegu7xjmuSIqHaKRCneVOfzad3ITwPSo'); // Replace with your token
+let balanceMonitorInterval = null;
+
+
+let isBotActive = false; // To keep track of bot's state
+
+bot.start((ctx) => {
+    ctx.reply('What would you like to do?', 
+        Markup.inlineKeyboard([
+            [Markup.button.callback('Balance💳', 'balance'), 
+             Markup.button.callback('Add APIKEY🔑', 'addApi')],
+            [Markup.button.callback(isBotActive ? 'STOP⏹️' : 'START▶️', 'toggleStart')]
+        ]).resize().oneTime()
+    );
+});
+
+
+// Create the scene
+const apiKeyWizard = new WizardScene(
+    'api_key_wizard',
+    (ctx) => {
+        ctx.reply('Please enter your API key:');
+        return ctx.wizard.next();
+    },
+    (ctx) => {
+        const apiKey = ctx.message.text;
+        console.log(apiKey)
+        if (!validateApiKey(apiKey)) {
+            ctx.reply('Invalid API key format. Please try again:');
+            return; // Stay at this step
+        }
+        ctx.wizard.state.apiKey = apiKey; // Temporarily store API key
+        ctx.reply('Please enter your API secret:');
+        return ctx.wizard.next();
+    },
+    (ctx) => {
+        const apiSecret = ctx.message.text;
+        if (!validateApiSecret(apiSecret)) {
+            ctx.reply('Invalid API secret format. Please try again:');
+            return; // Stay at this step
+        }
+        // Save API key and secret in session
+        ctx.session.apiKey = ctx.wizard.state.apiKey;
+        ctx.wizard.state.apiSecret = apiSecret; // Temporarily store API secret
+        ctx.reply('Testing API key and secret...');
+        testApiKey(ctx.wizard.state.apiKey, ctx.wizard.state.apiSecret)
+            .then(isValid => {
+                if (!isValid) {
+                    ctx.reply('Failed to validate API key and secret with Binance. Please try again.');
+                    return ctx.scene.leave();
+                }
+                // If valid, proceed to save credentials
+                ctx.session.credentials = ctx.session.credentials || [];
+                ctx.session.credentials.push({ apiKey: ctx.wizard.state.apiKey, apiSecret: ctx.wizard.state.apiSecret });
+                saveCredentials(ctx.session.credentials); // Save to file
+                ctx.reply('API key and secret tested and saved successfully.');
+                return ctx.scene.leave();
+            })
+            .catch(error => {
+                ctx.reply('Error testing API key and secret. Please try again.');
+                console.error(error);
+                return ctx.scene.leave();
+            });
+    }
+);
+bot.action('balance', async (ctx) => {
+    await ctx.answerCbQuery();  // Acknowledge the callback query
+
+    const credentials = await readCredentials();
+    if (credentials.length === 0) {
+        ctx.reply('No API credentials found.');
+        return;
+    }
+
+    const balancePromises = credentials.map((credential, index) => 
+        fetchBalance(credential.apiKey, credential.apiSecret)
+            .then(usdtBalanceInfo => {
+                if (usdtBalanceInfo.balance !== 'Error' && usdtBalanceInfo.crossUnPnl !== 'Error') {
+                    const totalBalance = parseFloat(usdtBalanceInfo.balance) + parseFloat(usdtBalanceInfo.crossUnPnl);
+                    return `<b>Account ${index + 1}:\nTotal USDT Balance</b>: ${totalBalance.toFixed(2)}\n\n`;
+                } else {
+                    return `<b>Account ${index + 1}:\nUnable to fetch balance.<b>\n\n`;
+                }
+            })
+    );
+
+    Promise.all(balancePromises)
+        .then(balanceInfos => {
+            const responseText = '<b>Balance:</b>\n\n' + balanceInfos.join('');
+            ctx.replyWithHTML(responseText);
+        })
+        .catch(error => {
+            console.error('Error fetching balances:', error);
+            ctx.reply('<b>⚠️⚠️An error occurred while fetching balances.⚠️⚠️</b>');
+        });
+});
+
+const thresholdWizard = new WizardScene(
+    'threshold_wizard',
+    (ctx) => {
+        ctx.reply('Please enter the threshold amount (X) for closing positions:');
+        return ctx.wizard.next();
+    },
+    (ctx) => {
+        ctx.wizard.state.threshold = parseFloat(ctx.message.text);
+        ctx.reply(`Threshold set to ${ctx.wizard.state.threshold}. Starting balance monitoring...`);
+        startBalanceMonitoring(ctx, ctx.wizard.state.threshold) // Start monitoring every 10 seconds
+        return ctx.scene.leave();
+    }
+);
+
+
+
+
+const stage = new Scenes.Stage([apiKeyWizard, thresholdWizard]);
+bot.use(session());
+bot.use(stage.middleware());
+
+bot.action('toggleStart', async (ctx) => {
+    await ctx.answerCbQuery();  // Acknowledge the callback query
+    isBotActive = !isBotActive;
+
+    if (isBotActive) {
+        ctx.scene.enter('threshold_wizard');
+    } else {
+        stopInterval();
+        ctx.reply('Balance monitoring stopped for all accounts.');
+    }
+
+
+    // Update the message with updated buttons
+    ctx.editMessageReplyMarkup(
+        Markup.inlineKeyboard([
+            [Markup.button.callback('Balance💳', 'balance'), 
+             Markup.button.callback('Add APIKEY🔑', 'addApi')],
+            [Markup.button.callback(isBotActive ? 'STOP⏹️' : 'START▶️', 'toggleStart')]
+        ]).resize()
+    );
+});
+
+
+bot.action('addApi', async (ctx) => {
+    try {
+        await ctx.answerCbQuery();  // Acknowledge the callback query
+        await ctx.scene.enter('api_key_wizard');
+    } catch (error) {
+        console.error('Error entering API key wizard:', error);
+    }
+});
+
+
+function stopInterval() {
+    if (balanceMonitorInterval) {
+        clearInterval(balanceMonitorInterval);
+        balanceMonitorInterval = null;
+    }
+}
+
+
+function startBalanceMonitoring(ctx, thresholdPercentage) {
+    balanceMonitorInterval = setInterval(async () => {
+        const credentials = await readCredentials();
+        if (credentials.length === 0) {
+            ctx.reply('No API Keys found, please register them through /start')
+            stopInterval()
+            return
+        }
+        for (const [index, credential] of credentials.entries()) {
+            const balanceInfo = await fetchBalance(credential.apiKey, credential.apiSecret);
+
+            if (balanceInfo.balance !== 'Error' && balanceInfo.crossUnPnl !== 'Error') {
+                const initialWalletBalance = parseFloat(balanceInfo.balance);
+                const currentTotalBalance = initialWalletBalance + parseFloat(balanceInfo.crossUnPnl);
+                const thresholdAmount = initialWalletBalance * thresholdPercentage / 100;
+
+                if (initialWalletBalance - currentTotalBalance >= thresholdAmount) {
+                    await ctx.reply(`❗Alert: Total balance for Account ${index + 1} dropped by more than ${thresholdPercentage}%. Current balance: ${currentTotalBalance.toFixed(2)}`);
+                    stopInterval()
+                    let closed = await closeAllOpenPositions(credential.apiKey, credential.apiSecret)
+                    if (closed){
+                        ctx.reply('All the orders has been closed, if you want to monitor your orders again go to /start and start it over')
+                    }
+                }
+            } else {
+                await ctx.reply(`Error fetching balance for Account ${index + 1}.`);
+            }
+        }
+    }, 10000); // Check every 10 seconds
+}
+
+
+bot.launch();
+console.log('Bot is running!')
